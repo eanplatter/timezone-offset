ab42656bc20984cc35acd6e0fe69a6d56a22881e
eanplatter <ean.platter@gmail.com>
2016-10-05 16:27:38 -0500

    fix arg issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var timezoneOffset = function() {
   var rawOffset = new Date().getTimezoneOffset()
-  var absValue = Math.abs(offset)
+  var absValue = Math.abs(rawOffset)
   return (rawOffset < 0 ? "+" : "-") + ("00" + Math.floor(absValue / 60)).slice(-2) + ":" + ("00" + (absValue % 60)).slice(-2);
 }
 
